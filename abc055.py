9f384fb075b15c49efc8ba18c54783629b69c1cc
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 17:19:11 +0900

    abc055

---
+++ abc055.py
@@ -0,0 +1,12 @@
+https://atcoder.jp/contests/abc055/submissions/16074103
+
+n = int(input())
+
+if n >= 15:
+    x = n * 800
+    y =( n // 15 ) * 200
+    answer = x - y
+    print(answer)
+else:
+    answer = n * 800
+    print(answer)
