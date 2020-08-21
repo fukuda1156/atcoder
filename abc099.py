acc0508041fdc38345b67f7cb452e9d9cf2300df
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 15:19:15 +0900

    abc099

---
+++ abc099.py
@@ -0,0 +1,10 @@
+https://atcoder.jp/contests/abc099/submissions/16071974
+
+n = int(input())
+
+if n >= 1000:
+    print("ABD")
+    # print("ABD" + str(n - 999).zfill(3))
+if n <= 999:
+    print("ABC")
+    # print("ABC" + str(n))
