20e9d453f6f8618a44c6cc25623fafc07efaa4ab
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 18:16:42 +0900

    abc097

--- abc043.py
+++ abc043.py
@@ -1,5 +1,5 @@
-# n = map(int, input())
-#
-# print sum(n + 1) * n / 2
+n = int(input())
 
+answer = (n + 1) * n / 2
 
+print(answer)
--- abc097.py
+++ abc097.py
@@ -1,15 +1,9 @@
-S = input()
-if S == 'SUN':
-    print('7')
-if S == 'MON':
-    print('6')
-if S == 'TUE':
-    print('5')
-if S == 'WED':
-    print('4')
-if S == 'THU':
-    print('3')
-if S == 'FRI':
-    print('2')
-if S == 'SAT':
-    print('1')
+a
+b
+c
+d
+
+a[m]
+b[m]
+c[m]
+d[m] <= d
