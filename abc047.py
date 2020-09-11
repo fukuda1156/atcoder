0beceebcb6ad0d9d12c355d6465aa99c8308125c
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 01:38:40 +0900

    abc047_a

--- abc047.py
+++ abc047.py
@@ -1,8 +1,8 @@
-a = int(input())
-b = int(input())
-c = int(input())
+# https://atcoder.jp/contests/abc047/submissions/16636980
+
+a, b, c = map(int, input().split())
 
 if a == b + c or b == a + c or c == a + b:
-  print("Yes")
+    print("Yes")
 else:
-  print("No")
+    print("No")
