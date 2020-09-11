5ddb5ea947f6007bca1d2afa1513a82fc9f7a9a4
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 01:16:27 +0900

    abc067_a

---
+++ abc067_a.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc067/submissions/16636738
+
+a, b = map(int, input().split())
+
+if (a + b) % 3 == 0 or b % 3 == 0 or a % 3 == 0:
+    print("Possible")
+else:
+    print("Impossible")
