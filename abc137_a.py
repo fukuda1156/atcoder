9dcb09a07495a9558c23ce6188b65ccd81061823
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 00:40:27 +0900

    abc137_a

--- abc137_a.py
+++ abc137_a.py
@@ -1,4 +1,4 @@
-https: // atcoder.jp / contests / abc137 / submissions / 16636243
+# https://atcoder.jp/contests/abc137/submissions/16636243
 
 a, b = map(int, input().split())
 print(max(a + b, a - b, a * b))
