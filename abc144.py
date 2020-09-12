96936980a7b78dcaa4b9a5e01faa2f42241fa5f8
fukuda1156 <69306438+fukuda1156@users.noreply.github.com>
2020-09-12 10:28:52 +0900

    Delete abc144.py

--- abc144.py
+++
@@ -1,9 +0,0 @@
-# https://atcoder.jp/contests/abc144/submissions/16074405
-
-a, b = map(int, input().split())
-
-if a >= 10 or b >= 10:
-    print('-1')
-else:
-    answer = a * b
-    print(answer)
