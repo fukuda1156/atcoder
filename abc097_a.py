6dac0f4fa9f3b6e8df70dbfe4d4f56950c66836a
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 10:27:24 +0900

    abc097_a

--- abc097_a.py
+++ abc097_a.py
@@ -2,7 +2,7 @@
 
 a, b, c, d = map(int, input().split())
 
-if abs(a-b) <= d and abs(b-c) <= d or abs(a-c) <= d:
+if abs(a - b) <= d and abs(b - c) <= d or abs(a - c) <= d:
     print('Yes')
 else:
     print('No')
