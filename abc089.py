17c6a391f6a55b27374a36fb5c8b99e5fc04e001
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:27:52 +0900

    abc089_a

--- abc089.py
+++
@@ -1,5 +0,0 @@
-# https://atcoder.jp/contests/abc089/submissions/16070702
-
-n = input()
-answer = int(n) // 3
-print(answer)
---
+++ abc089_a.py
@@ -0,0 +1,4 @@
+# https://atcoder.jp/contests/abc089/submissions/16730166
+
+n = input()
+print(int(n) // 3)
