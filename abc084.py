9a7f4a2db5f605b6685f3fc189e8ced8188ad05d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:32:04 +0900

    abc084_a

--- abc084.py
+++
@@ -1,7 +0,0 @@
-# https://atcoder.jp/contests/abc084/submissions/16272693
-
-m = int(input())
-
-x = 48 - m
-
-print(x)
---
+++ abc084_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc084/submissions/16730260
+
+m = int(input())
+
+print(48 - m)
