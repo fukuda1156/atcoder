ab555cb1db13de51b15cb5e2c8dc69fe18d2e2fd
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:35:45 +0900

    abc074_a

--- abc074.py
+++
@@ -1,9 +0,0 @@
-# https://atcoder.jp/contests/abc074/submissions/16273197
-
-n = int(input())
-a = int(input())
-
-y = n * n
-x = y - a
-
-print(x)
---
+++ abc074_a.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abc074/submissions/16730333
+
+n = int(input())
+a = int(input())
+
+print(n * n - a)
