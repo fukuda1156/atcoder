dd6d34df399ec8bde253c46354ecb11d0ab72818
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:23:21 +0900

    abc043_a

--- abc043.py
+++
@@ -1,7 +0,0 @@
-# https://atcoder.jp/contests/abc043/submissions/16077989
-
-n = int(input())
-
-answer = int((n + 1) * n / 2)
-
-print(answer)
---
+++ abc043_a.py
@@ -0,0 +1,4 @@
+# https://atcoder.jp/contests/abc043/submissions/16730037
+
+n = int(input())
+print(int((n + 1) * n / 2))
