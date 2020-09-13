979a7487e4f42c3db0eea9d7e75c82214c91057e
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:33:06 +0900

    abc076_a

--- abc076.py
+++
@@ -1,8 +0,0 @@
-# https://atcoder.jp/contests/abc076/submissions/16273005
-
-r = int(input())
-g = int(input())
-
-x = 2 * g - r
-
-print(x)
---
+++ abc076_a.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abc076/submissions/16730284
+
+r = int(input())
+g = int(input())
+
+print(2 * g - r)
