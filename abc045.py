892c1bf31b504582c8a7112d9dad9c81af649736
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 01:24:34 +0900

    abc045_a

--- abc045.py
+++
@@ -1,9 +0,0 @@
-# https://atcoder.jp/contests/abc045/submissions/16637050
-
-a = int(input())
-b = int(input())
-h = int(input())
-
-answer = (a + b) * h // 2
-
-print(answer)
---
+++ abc045_a.py
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc045/submissions/16730089
+
+a = int(input())
+b = int(input())
+h = int(input())
+
+print((a + b) * h // 2)
