153459772aa004ace8c2c86886e62ecb0cf59b57
BuildTools <unconfigured@null.spigotmc.org>
2020-10-18 01:04:05 +0900

    abc129_b

---
+++ abc129_b.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc129/submissions/17487197
+
+import sys
+
+N = int(input())
+w_list = list(map(int, input().split()))
+
+answer = sum(w_list)
+
+for i in range(len(w_list)):
+    tmp = abs(sum(w_list[0:i]) - sum(w_list[i:]))
+    if answer > tmp: answer = tmp
+
+print(answer)
