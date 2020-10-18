64af28910ee44dbb9b90115a76396a56601d098b
BuildTools <unconfigured@null.spigotmc.org>
2020-10-18 10:06:15 +0900

    abc136_a

--- abc136_a.py
+++ abc136_a.py
@@ -1,10 +1,11 @@
+# https://atcoder.jp/contests/abc136/submissions/17491794
+
 a, b, c = map(int, input().split())
 
 ab = a - b
-cab = c - ab
+answer = c - ab
 
-if cab >= 0:
-    print(0)
-
-if cab < 0:
-    print(cab)
+if answer <= 0:
+    print("0")
+else:
+    print(answer)
