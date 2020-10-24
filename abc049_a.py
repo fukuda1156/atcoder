f5ffe4ca1445595a9385076e6461aacf00bfc2c4
BuildTools <unconfigured@null.spigotmc.org>
2020-10-25 01:45:19 +0900

    abc049_a

--- abc049_a.py
+++ abc049_a.py
@@ -1,6 +1,10 @@
-c = input()
+# https://atcoder.jp/contests/abc049/submissions/17643223
 
-if c == "a" or "e" or "i" or "o" or "u":
+C = input()
+
+vowel = ["a", "i", "u", "e", "o"]
+
+if C in vowel:
     print("vowel")
 else:
     print("consonant")
