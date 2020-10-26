dfdb7d3cc3601721d19cca79557b1dfe4aded6bd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 21:05:31 +0900

    abc155_b

---
+++ abc155_b.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc155/submissions/17673637
+
+N = int(input())
+list_A = list(map(int, input().split()))
+
+for i in list_A:
+    if i % 2 == 0 and i % 3 != 0 and i % 5 != 0:
+        print('DENIED')
+        exit()
+else:
+    print("APPROVED")
