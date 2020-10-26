bf26c1e407fa86f1ebb75c3e36eda19b81b30ddd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 20:32:12 +0900

    abc060_a

--- abc060_a.py
+++ abc060_a.py
@@ -1,6 +1,8 @@
+# https://atcoder.jp/contests/abc060/submissions/17672980
+
 A, B, C = input().split()
 
 if A[-1] == B[0] and B[-1] == C[0]:
     print("YES")
 else:
-    print("No")
+    print("NO")
