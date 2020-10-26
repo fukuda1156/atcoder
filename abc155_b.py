e37ada05af8530e523e907230d43da2746ccb425
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 21:07:19 +0900

    abc155_b

--- abc155_b.py
+++ abc155_b.py
@@ -1,10 +1,12 @@
-# https://atcoder.jp/contests/abc155/submissions/17673637
+# https://atcoder.jp/contests/abc155/submissions/17673684
 
 N = int(input())
 list_A = list(map(int, input().split()))
 
 for i in list_A:
-    if i % 2 == 0 and i % 3 != 0 and i % 5 != 0:
+    if i % 2 == 0 and \
+            i % 3 != 0 and \
+            i % 5 != 0:
         print('DENIED')
         exit()
 else:
