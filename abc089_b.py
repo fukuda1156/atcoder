f08b4b3df1cc51bf77899807ab3ab87f4fa1d18c
BuildTools <unconfigured@null.spigotmc.org>
2020-10-30 00:20:21 +0900

    abc089_b

---
+++ abc089_b.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc089/submissions/17722561
+
+N = int(input())
+S = input().split()
+
+if "G" and "W" and "P" and "Y" in S:
+    print("Four")
+else:
+    print("Three")
