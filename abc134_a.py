00cbcce51ebb4efb75627a169b844213609ca503
BuildTools <unconfigured@null.spigotmc.org>
2020-11-26 01:27:04 +0900

    abc134_a

---
+++ abc134_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc134/submissions/18409577
+
+r = int(input())
+
+print(3 * r * r)
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,15 +1,16 @@
 # https://atcoder.jp/contests/abc145/tasks/abc145_a
 
 def answer(r: int) -> str:
-    return str(r * r)
+    return str(3 * r * r)
 
 
 def test_入力例1():
-    assert answer(2) == "4"
+    assert answer(4) == "48"
 
 
 def test_入力例2():
-    assert answer(100) == "10000"
+    assert answer(15) == "675"
 
-# def test_入力例3():
-#     assert answer() == "0"
+
+def test_入力例3():
+    assert answer(80) == "19200"
