abb162a63829bed398124f9429fb7a58d630ddbd
BuildTools <unconfigured@null.spigotmc.org>
2020-11-26 23:41:20 +0900

    abc167_a

---
+++ abc167_a.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc167/submissions/18423025
+
+S = input()
+T = input()
+
+if S == T[0:-1]:
+    print("Yes")
+else:
+    print("No")
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,18 +1,17 @@
-# https://atcoder.jp/contests/abc163/tasks/abc163_a
+# https://atcoder.jp/contests/abc167/tasks/abc167_a
 
-import math
-
-
-def answer(R: int) -> float:
-    return 2 * math.pi * R
-
+def answer(S: str, T: str) -> str:
+    if S == T[0:-1]:
+        return "Yes"
+    else:
+        return "No"
 
 def test_入力例1():
-    assert answer(1) == 6.28318530717958623200
-
+    assert answer("chokudai", "chokudaiz") == "Yes"
 
 def test_入力例2():
-    assert answer(73) == 458.67252742410977361942
+    assert answer("snuke", "snekee") == "No"
 
-# def test_入力例3():
-#     assert answer(-1) == "0"
+
+def test_入力例3():
+    assert answer("a", "aa") == "Yes"
