93028e58eb1eff1468e8b91adc421331e1752845
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 16:23:30 +0900

    abc150_a

---
+++ abc150_a.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc150/submissions/18430649
+
+K, X = map(int, input().split())
+
+if 500 * K >= X:
+    print("Yes")
+else:
+    print("No")
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,21 +1,19 @@
-# https://atcoder.jp/contests/abc104/submissions/18430485
+# https://atcoder.jp/contests/abc150/tasks/abc150_a
 
-def answer(R: int) -> str:
-    if 0 <= R < 1200:
-        return ("ABC")
-    if 1200 <= R < 2800:
-        return ("ARC")
-    if 2800 <= R:
-        return ("AGC")
+def answer(K: int, X: int) -> str:
+    if 500 * K >= X:
+        return ("Yes")
+    else:
+        return ("No")
 
 
 def test_入力例1():
-    assert answer(1199) == "ABC"
+    assert answer(2, 900) == "Yes"
 
 
 def test_入力例2():
-    assert answer(1200) == "ARC"
+    assert answer(1, 501) == "No"
 
 
 def test_入力例3():
-    assert answer(4208) == "AGC"
+    assert answer(4, 2000) == "Yes"
