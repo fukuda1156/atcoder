29ad836e4f1831a6e6b75f0893cf3832f418449b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 12:50:52 +0900

    abc160_a

--- test_atcoder.py
+++ test_atcoder.py
@@ -1,27 +1,19 @@
-# https://atcoder.jp/contests/abc165/tasks/abc165_a
+# https://atcoder.jp/contests/abc160/tasks/abc160_a
 
-# def answer(K: int, A: int, B: int) -> str:
-#     for i in range(0, 1000, K):
-#         if A <= i <= B:
-#             return ("OK")
-#             exit()
-#     else:
-#         return ("NG")
-
-def answer(K: int, A: int, B: int) -> str:
-    if B - A >= B % K:
-        return ("OK")
+def answer(S: list) -> str:
+    if S[2] == S[3] and S[4] == S[5]:
+        return ("Yes")
     else:
-        return ("NG")
+        return ("No")
 
 
 def test_入力例1():
-    assert answer(7, 500, 600) == "OK"
+    assert answer(['s', 'i', 'p', 'p', 'u', 'u']) == "Yes"
 
 
 def test_入力例2():
-    assert answer(4, 5, 7) == "NG"
+    assert answer(['i', 'p', 'h', 'o', 'n', 'e']) == "No"
 
 
 def test_入力例3():
-    assert answer(1, 11, 11) == "OK"
+    assert answer(['c', 'o', 'f', 'f', 'e', 'e']) == "Yes"
