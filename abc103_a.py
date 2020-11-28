c3661cdef531d4ac82779126ebcff3eb49258314
BuildTools <unconfigured@null.spigotmc.org>
2020-11-28 10:05:12 +0900

    abc103_a

---
+++ abc103_a.py
@@ -0,0 +1,4 @@
+# https://atcoder.jp/contests/abc103/submissions/18441435
+
+A123 = list(map(int, input().split()))
+print(max(A123) - min(A123))
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,19 +1,16 @@
-# https://atcoder.jp/contests/abc118/tasks/abc118_a
+# https://atcoder.jp/contests/abc103/tasks/abc103_a
 
-def answer(A: int, B: int) -> str:
-    if B % A == 0:
-        return str(A + B)
-    else:
-        return str(B - A)
+def answer(A123: list) -> str:
+    return str(max(A123) - min(A123))
 
 
 def test_入力例1():
-    assert answer(4, 12) == "16"
+    assert answer([1, 6, 3]) == "5"
 
 
 def test_入力例2():
-    assert answer(8, 20) == "12"
+    assert answer([11, 5, 5]) == "6"
 
 
 def test_入力例3():
-    assert answer(1, 1) == "2"
+    assert answer([100, 100, 100]) == "0"
