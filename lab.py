ea9f272ad97c6af20e33f5506215b0f1b4692b28
BuildTools <unconfigured@null.spigotmc.org>
2020-12-14 07:28:54 +0900

    abc136_b

--- lab.py
+++ lab.py
@@ -1,5 +1,19 @@
-s = 0
-n = 21
-for i in range(1, n):
-    s += i
-print(s)
+import math
+import struct
+
+odd = 0
+
+num = 0b1010
+print(f'{num:d}')
+
+# for i in range(11, 0, -1):
+#     num = "0b" + str(i)
+#     print(f'{int(num):d}')
+
+
+# int(str(i), base=2)
+# print(i)
+#     if i % 2 != 0:
+#         print(i)
+# #         odd += 1
+# print(odd)
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,20 +1,21 @@
-def answer(ABC: list, K: int) -> int:
-    if K == 1:
-        return int(max(ABC) * 2 + (sum(ABC) - max(ABC)))
-    else:
-        return int(max(ABC) ** K + (sum(ABC) - max(ABC)))
+def answer(N: int) -> int:
+    odd = 0
+    for i in range(N, 0, -1):
+        if len(str(i)) % 2 == 1:
+            odd += 1
+    return odd
 
 
 def test_入力例1():
-    assert answer([5, 3, 11], 1) == 30
+    assert answer(11) == 9
 
 
 def test_入力例2():
-    assert answer([3, 3, 4], 2) == 22
+    assert answer(136) == 46
 
-# def test_入力例3():
-#     assert answer() == ""
-#
-#
+
+def test_入力例3():
+    assert answer(100000) == 90909
+
 # def test_入力例4():
 #     assert answer() == ""
