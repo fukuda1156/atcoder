bc52da37f0c1cea8becf11f5961c4381cda3577d
BuildTools <unconfigured@null.spigotmc.org>
2020-12-13 22:32:26 +0900

    abc068_b

---
+++ abc068_b.py
@@ -0,0 +1,26 @@
+# https://atcoder.jp/contests/abc068/submissions/18760104
+
+N = int(input())
+
+for i in range(N, 0, -1):
+    if i == 1:
+        print(i)
+        break
+    if i == 2:
+        print(i)
+        break
+    if i == 4:
+        print(i)
+        break
+    if i == 8:
+        print(i)
+        break
+    if i == 16:
+        print(i)
+        break
+    if i == 32:
+        print(i)
+        break
+    if i == 64:
+        print(i)
+        break
