0dccd91399bb408195bdf30ec55ce7525b174413
BuildTools <unconfigured@null.spigotmc.org>
2020-12-18 18:57:11 +0900

    abc012_1

---
+++ abc012_1.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc012/submissions/18842147
+
+A, B = map(int, input().split())
+
+print(f"{B} {A}")
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,19 +1,16 @@
-def answer(M: int, D: int) -> str:
-    if M % D == 0:
-        return "YES"
-    else:
-        return "NO"
+def answer(A: int, B: int) -> str:
+    return f"{B} {A}"
+
 
 def test_入力例1():
-    assert answer(1, 1) == "YES"
+    assert answer(1, 2) == "2 1"
 
 
 def test_入力例2():
-    assert answer(2, 29) == "NO"
+    assert answer(5, 5) == "5 5"
 
-
-def test_入力例3():
-    assert answer(12, 6) == "YES"
+# def test_入力例3():
+#     assert answer(12, 6) == "YES"
 
 # def test_入力例4():
 #     assert answer(50) == 625
