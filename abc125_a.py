c252c071be927347bd3bf842bac66676a352b8f8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 21:34:34 +0900

    abc125_a

---
+++ abc125_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc125/submissions/18879317
+
+A, B, T = map(int, input().split())
+
+print((T // A) * B)
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,19 +1,17 @@
-def answer(A: int, B: int) -> int:
-    if A == B:
-        return A + B
-    if A != B:
-        return max(A, B) + (max(A, B) - 1)
+def answer(A: int, B: int, T: int) -> int:
+    return (T // A) * B
 
 
 def test_入力例1():
-    assert answer(5, 3) == 9
+    assert answer(3, 5, 7) == 10
 
 
 def test_入力例2():
-    assert answer(6, 6) == 12
+    assert answer(3, 2, 9) == 6
 
-# def test_入力例3():
-#     assert answer(12, 6) == "YES"
+
+def test_入力例3():
+    assert answer(20, 20, 19) == 0
 
 # def test_入力例4():
 #     assert answer(50) == 625
