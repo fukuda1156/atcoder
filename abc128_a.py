ab2d2b1d4094da6be0fdac5a9f6d09fca219ff52
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 23:33:50 +0900

    abc123_a

---
+++ abc128_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc128/submissions/18894366
+
+A, P = map(int, input().split())
+
+print((A * 3 + P) // 2)
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,17 +1,17 @@
-def answer(A: int, B: int, T: int) -> int:
-    return (T // A) * B
+def answer(A: int, P: int) -> int:
+    return (A * 3 + P) // 2
 
 
 def test_入力例1():
-    assert answer(3, 5, 7) == 10
+    assert answer(1, 3) == 3
 
 
 def test_入力例2():
-    assert answer(3, 2, 9) == 6
+    assert answer(0, 1) == 0
 
 
 def test_入力例3():
-    assert answer(20, 20, 19) == 0
+    assert answer(32, 21) == 58
 
 # def test_入力例4():
 #     assert answer(50) == 625
