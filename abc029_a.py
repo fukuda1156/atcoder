e1735dbfe752ab5ee4b0c7080b801f8f31536dbc
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 23:59:57 +0900

    abc029_a

---
+++ abc029_a.py
@@ -0,0 +1,5 @@
+# https://atcoder.jp/contests/abc029/submissions/18895214
+
+W = input()
+
+print(f"{W}s")
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,17 +1,16 @@
-def answer(A: int, P: int) -> int:
-    return (A * 3 + P) // 2
+def answer(W: str) -> str:
+    return f"{W}s"
 
 
 def test_入力例1():
-    assert answer(1, 3) == 3
+    assert answer("dog") == "dogs"
 
 
 def test_入力例2():
-    assert answer(0, 1) == 0
+    assert answer("chokudai") == "chokudais"
 
-
-def test_入力例3():
-    assert answer(32, 21) == 58
+# def test_入力例3():
+#     assert answer(32, 21) == 58
 
 # def test_入力例4():
 #     assert answer(50) == 625
