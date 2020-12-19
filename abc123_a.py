bb3c63475223695ca6977d4a0985f6ef701636e8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-19 23:23:44 +0900

    abc123_a

---
+++ abc123_a.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc123/submissions/18893968
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+k = int(input())
+
+if e - a <= k:
+    print("Yay!")
+else:
+    print(":(")
