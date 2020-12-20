0362a16e47bdd1912686406aa7e505fea031cc34
BuildTools <unconfigured@null.spigotmc.org>
2020-12-20 10:06:44 +0900

    abc112_a

---
+++ abc112_a.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc112/submissions/18900627
+
+N = int(input())
+
+if N != 1:
+    A = int(input())
+    B = int(input())
+
+if N == 1:
+    print("Hello World")
+
+if N != 1:
+    print(A + B)
