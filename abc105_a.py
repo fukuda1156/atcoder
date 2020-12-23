67f964070acbb907868079489402c93135d9fba6
BuildTools <unconfigured@null.spigotmc.org>
2020-12-23 09:56:46 +0900

    abc105_a

---
+++ abc105_a.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc105/submissions/18945714
+
+N, K = map(int, input().split())
+
+if N % K == 0:
+    print("0")
+else:
+    print("1")
--- test_atcoder.py
+++ test_atcoder.py
@@ -1,16 +1,20 @@
-def answer(W: str) -> str:
-    return f"{W}s"
+def answer(N: int, K: int) -> int:
+    if N % K == 0:
+        return 0
+    else:
+        return 1
 
 
 def test_入力例1():
-    assert answer("dog") == "dogs"
+    assert answer(7, 3) == 1
 
 
 def test_入力例2():
-    assert answer("chokudai") == "chokudais"
+    assert answer(100, 10) == 0
 
-# def test_入力例3():
-#     assert answer(32, 21) == 58
+
+def test_入力例3():
+    assert answer(1, 1) == 0
 
 # def test_入力例4():
 #     assert answer(50) == 625
