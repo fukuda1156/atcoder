5e3fdbbb2d3485a60a7bb8e9198eff717345c72f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 10:25:48 +0900

    abc032_a

---
+++ abc032_a.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc032/submissions/19501907
+
+a = int(input())
+b = int(input())
+n = int(input())
+
+for i in range(n, n + a * b + 1):
+  if i % a == 0 and i % b == 0:
+    print(i)
+    break
