8a3510ed6084f51479c0e855dfc3d831907186eb
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 09:46:27 +0900

    abc096_a

---
+++ abc096_a.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc096/submissions/19516825
+
+a, b = map(int, input().split())
+
+if a > b:
+    print(a - 1)
+else:
+    print(a)
--- abc181_b.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-
-a = [list(map(int, input().split())) for l in range(N)]
-
-print(a)
