af425714148df249c1286acf17396200434bc1c9
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 10:07:44 +0900

    abc073_b

--- abc073_b.py
+++ abc073_b.py
@@ -1,4 +1,4 @@
-# https://atcoder.jp/contests/abc073/submissions/20640254
+# https://atcoder.jp/contests/abc073/submissions/20640269
 
 N = int(input())
 l = [list(map(int, input().split())) for l in range(N)]
@@ -6,6 +6,6 @@ l = [list(map(int, input().split())) for l in range(N)]
 answer = 0
 
 for i in l:
-    answer += 1 + i[1] - i[0]
+    answer += 1 + max(i) - min(i)
 
 print(answer)
